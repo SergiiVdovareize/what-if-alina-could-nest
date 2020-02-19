12fac0a11aec48530b9e5ec4ef99767053cd8efa
Sergey Vdovareize <servdo@gmail.com>
2020-02-19 20:35:43 +0200

    docs

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "what-if-alina-could-nest",
+  "version": "0.1.0",
+  "description": "rest api on nest.js",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-02-19T18:35:04.899Z",
+    "url": "http://apidocjs.com",
+    "version": "0.19.1"
+  }
+});
--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -2,26 +2,53 @@ import { Controller, Post, Res, HttpStatus, Body, Get, Param, NotFoundException,
 import { CarService } from "./car.service";
 import { CreateCarDTO } from "./dto/car.dto";
 
-@Controller('car')
+@Controller('cars')
 export class CarController {
     constructor(private carService: CarService) { }
 
+    /**
+     * @api {post} /cars Create new car
+     * @apiGroup Cars
+     * @apiName New car
+     * @apiVersion 0.1.0
+     * 
+     * @apiParam {String} name Car name
+     * @apiParam {Number} price Car price
+     * @apiParam {Manufacturer} [manufacturer] Car manufacturer
+     * @apiParam {Date} [firstRegistrationDate] Car registration date
+     * @apiParam {Owner[]} [owners] List of owners
+     */
     @Post('/')
     async createCar(@Res() res, @Body() createCarDTO: CreateCarDTO) {
         const car = await this.carService.createCar(createCarDTO)
         return res.status(HttpStatus.OK).json(car);
     }
 
+    /**
+     * @api {get} /cars Get car list
+     * @apiGroup Cars
+     * @apiName Car list
+     * @apiVersion 0.1.0
+     */
     @Get('/')
     async getCars(@Res() res) {
         const cars = await this.carService.getCars();
         return res.status(HttpStatus.OK).json(cars);
     }
 
+    /**
+     * @api {get} /cars/:carId Get a single car
+     * @apiGroup Cars
+     * @apiName Show car
+     * @apiVersion 0.1.0
+     * 
+     * @apiParam {ObjectId} id Car id
+     * @apiParam {String=name,price,manufacturer} select field to show
+     */
     @Get('/:carId')
     async getCar(@Res() res, @Param('carId') carId, @Query('select') select) {
         const car = await this.carService.getCar(carId);
-        const availableFields = ['name', 'price', 'manufacturer', 'owners']
+        const availableFields = ['name', 'price', 'manufacturer']
 
         if (select) {
             if (car[select] && availableFields.indexOf(select) != -1) {
@@ -34,6 +61,14 @@ export class CarController {
         return res.status(HttpStatus.OK).json(car);
     }
 
+    /**
+     * @api {delete} /cars/:carId Delete existing car
+     * @apiGroup Cars
+     * @apiName Delete car
+     * @apiVersion 0.1.0
+     * 
+     * @apiParam {ObjectId} carId Car ID
+     */
     @Delete('/:carId')
     async deleteCar(@Res() res, @Param('carId') carId) {
         await this.carService.deleteCar(carId);
@@ -43,12 +78,30 @@ export class CarController {
         });
     }
 
+    /**
+     * @api {put} /cars/:carId Update existing car
+     * @apiGroup Cars
+     * @apiName Update car
+     * @apiVersion 0.1.0
+     * 
+     * @apiParam {String} name Car name
+     * @apiParam {Number} price Car price
+     * @apiParam {Manufacturer} [manufacturer] Car manufacturer
+     * @apiParam {Date} [firstRegistrationDate] Car registration date
+     * @apiParam {Owner[]} [owners] List of owners
+     */
     @Put('/:carId')
     async updateCar(@Res() res, @Body() createCarDTO: CreateCarDTO, @Param('carId') carId) {
         const updatedCar = await this.carService.updateCar(carId, createCarDTO);
         return res.status(HttpStatus.OK).json(updatedCar);
     }
 
+    /**
+     * @api {post} /cars/refresh Remove old owners and apply discount to old cars
+     * @apiGroup Cars
+     * @apiName Refresh cars
+     * @apiVersion 0.1.0
+     */
     @Post('/refresh')
     async refresh(@Res() res) {
         const discount = 20
